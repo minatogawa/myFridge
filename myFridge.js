13df01c6316f22eadf2bc7016d37b2517315ba5c
Minatogawa <vinicius@cyclum.in>
2016-04-08 13:18:01 -0300

    finished basic application of myFridge

--- myFridge.js
+++ myFridge.js
@@ -15,6 +15,35 @@ if (Meteor.isClient) {
 			});
 		}
 	});
+	Template.fridge.onRendered(function(){
+		var templateInstance = this;
+		templateInstance.$('#fridge').droppable({
+			drop:function(evt,ui){
+				var query = {_id: ui.draggable.data('id')};
+				var changes = { $set: {place:'fridge'}};
+				Products.update(query, changes);
+			}
+		});
+	});
+
+	Template.productList.onRendered(function(){
+		var templateInstance = this;
+		templateInstance.$('#supermarket').droppable({
+			drop:function(evt, ui){
+				var query = {_id:ui.draggable.data('id')};
+				var changes = {$set: {place:'supermarket'}};
+				Products.update(query, changes);
+			}
+		});
+	});
+
+	Template.productListItem.onRendered(function(){
+		var templateInstance = this;
+		templateInstance.$('.draggable').draggable({
+			cursor:'move',
+			helper:'clone',
+		});
+	});
 }
 if (Meteor.isServer) {
  //...
